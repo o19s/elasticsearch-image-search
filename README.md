7d88eb3efef7fa1e987dedccbf7989d776c55b94
Doug Turnbull <softwaredoug@gmail.com>
2015-06-12 10:41:00 -0400

    base image tokenization of raw rgb

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Python 2.7 project for toying with stupid image search ideas with Elasticsearch
+
+Use the requirements.txt with a virtualenv:
+
+```bash
+virtualenv venv
+pip install requirements.txt
+python tokenizeImages.py
+```
---
+++ tokenizeImage.py
@@ -0,0 +1,27 @@
+
+from elasticsearch import Elasticsearch
+from PIL import Image
+import os
+
+es = Elasticsearch()
+
+def images():
+    for f in os.listdir('img'):
+        fName, fExt = os.path.splitext(f)
+        if fExt == '.jpg':
+            imgId = fName
+            imgF = Image.open(os.path.join('img', f))
+            yield (imgId, imgF)
+
+
+def tokens(imgF):
+    for idx, (r, g, b) in enumerate(imgF.getdata()):
+        yield "%s_%s_%s_%s" % (idx,  r, g, b)
+
+def indexImg(imgId, tokens):
+    es.index('images', doc_type='image', id=imgId, body={'bmp': tokens})
+
+
+
+for imgId, img in images():
+    indexImg(imgId, [token for token in tokens(img)])
